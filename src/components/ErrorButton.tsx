75059f798126b222174e65b5dd6630222727ee36
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-02-28 10:47:44 +0100

    Create error button component

---
+++ src/components/ErrorButton.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const Back = styled.button`
+  padding: 10px;
+  background: #f2f2f2;
+  border-radius: 10px;
+  transition: all 0.3s ease;
+  border: 0;
+
+  &:hover {
+    background: #e2e2e2;
+    transform: scale(1.05);
+    cursor: pointer;
+  }
+`
+
+export const ErrorButton = () => {
+  return (
+    <Link to="/" style={{ textDecoration: 'none', color: '#282c34' }}>
+      <Back>⬅ Back to list</Back>
+    </Link>
+  )
+}
